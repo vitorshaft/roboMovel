d395ab8fb586ac02913026967ff71df0f08267f7
Vitor Rasp <vtrshaft@gmail.com>
2020-05-25 18:45:11 -0300

    corrigida ambiguidade de arcTg

--- plan/rota.py
+++ plan/rota.py
@@ -4,15 +4,21 @@ class plan:
 	def __init__(self):
 		pass
 	def traj(self,xo,yo,xr,yr):
-		self.a = float(yo-yr)/float(xo-xr)
-		self.teta = math.atan(self.a)
-		self.alfa = math.degrees(self.teta)
-		if (xo < 0 and yo > 0):
-			self.alfa = self.alfa+180
-		elif (xo < 0 and yo < 0):
-			self.alfa = self.alfa+180
-		elif (xo > 0 and yo < 0):
-			self.alfa = self.alfa+ 360
-		#self.b = yr -(self.a*xr)
-		self.h = math.hypot((yo-yr),(xo-xr))
+		self.dx = float(xo-xr)
+		self.dy = float(yo-yr)
+		self.tg = self.dy/self.dx
+		self.h = math.hypot(self.dx,self.dy)
+		self.teta = math.atan(self.tg)
+		self.seno = self.dy/self.h
+		self.cos = self.dx/self.h
+		if (self.seno > 0 and self.cos > 0):	#Q1
+			self.alfa = math.degrees(math.atan(self.tg))
+		elif (self.seno > 0 and self.cos < 0):	#Q2
+			self.alfa = math.degrees(math.acos(self.cos))
+		elif (self.seno < 0 and self.cos <0):	#Q3
+			self.alfa = math.degrees(math.atan(self.tg))+180
+		elif (self.seno < 0 and self.cos >0):	#Q4
+			self.alfa = math.degrees(math.atan(self.tg))+360
+		
+		print(self.alfa,self.h)
 		return [self.alfa,self.h]
--- planPlay.py
+++ planPlay.py
@@ -18,14 +18,15 @@ while(True):
 	xis = float(input("Entre com o X do obj: "))
 	ips = float(input("Entre com o Y do obj: "))
 	dPolar = p.traj(xis,ips,xR,yR) #[angulo,dist]
-	print ("virando %d graus e andando %d cm"%(dPolar[0],dPolar[1]))
+	print ("virando para %d graus e andando %d cm"%(dPolar[0],dPolar[1]))
+	print(dPolar[0]-teta)
 	if (dPolar[0]-teta > 0):
 		x.esqRad(dPolar[0]-teta)
 	elif (dPolar[0]-teta < 0):
 		x.dirRad((dPolar[0]-teta)*-1)
 	x.parar()
 	time.sleep(1)
-	x.frente(dPolar[1])
+	#x.frente(dPolar[1])
 	sen = math.sin(math.radians(dPolar[0]))
 	cos = math.cos(math.radians(dPolar[0]))
 	coor.writeLoc(arquivoRobo,xis,ips,sen,cos,dPolar[0]) #(arq,x,y,sen,cos,teta)
